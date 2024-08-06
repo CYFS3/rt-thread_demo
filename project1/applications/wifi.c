7c29d06f9374974695450ba842f8397f9190912d
CYFS <2805686936@qq.com>
2024-08-06 16:40:41 +0800

    [add]wifi join

--- project1/applications/wifi.c
+++ project1/applications/wifi.c
@@ -1,12 +1,25 @@
 #include <rtthread.h>
 #include <rtdevice.h>
 
-int wifi_join_init(void)
+void wifi_join_init(void * parameter)
 {
+	rt_thread_mdelay(1000);
+	rt_kprintf("wifi join\n");
 	system("wifi scan");    
-	system("wifi join CYFS 12345678");
-	return RT_EOK;
+	char arr[] = "wifi join CYFS 12345678";
+	system(arr);
+
 }
 
-
+int wifi_join(void)
+{
+	rt_thread_t tid;
+	tid = rt_thread_create("wifi_join", wifi_join_init, RT_NULL, 2048, 10, 10);
+	if (tid != RT_NULL)
+	{
+		rt_thread_startup(tid);
+	}
+	return RT_EOK;
+}
+INIT_APP_EXPORT(wifi_join);
 
